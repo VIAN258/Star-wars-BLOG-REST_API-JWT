e4a31c9b5fdde8dedcfb05df33a049e4342a0df5
VIAN258 <angelmorales891@gmail.com>
2023-02-17 22:34:23 +0000

    model

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,77 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from eralchemy2 import render_er
 
-db = SQLAlchemy()
+Base = declarative_base()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(Integer, primary_key=True)
+    planet_name = db.Column(String(250))
+   
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Ship(Base):
+    __tablename__ = 'ship'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    ships_name = Column(String(250))
+   
+
+class Character(Base):
+    __tablename__ = 'character'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(Integer, primary_key=True)
+    characters_name = db.Column(String(250))
+  
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(Integer, primary_key=True)
+    user_id = db.Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship(User)
+    character_id = db.Column(Integer, ForeignKey('character.id'), nullable=False)
+    character = relationship(Character)
+    planet_id = db.Column(Integer, ForeignKey('planet.id'), nullable=False)
+    planet = relationship(Planet)
+    ship_id = db.Column(Integer, ForeignKey('ship.id'), nullable=False)
+    ship = relationship(Ship)
+    type_id = db.Column(Integer, ForeignKey('type.id'), nullable=False)
+    
+ 
+class Type(Base):
+    __tablename__ = 'type'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(Integer, primary_key=True)
+    character_id = db.Column(Integer, ForeignKey('character.id'))
+    planet_id = db.Column(Integer, ForeignKey('planet.id'))
+    ship_id = db.Column(Integer, ForeignKey('ship.id'))
+    
+    
+    
+   
+
+    def to_dict(self):
+        return {}
+
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
