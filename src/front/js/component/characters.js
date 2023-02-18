df9a5b87e19d9b5015e149fbe625725879fd963c
VIAN258 <angelmorales891@gmail.com>
2023-02-18 15:43:12 +0000

    test

--- src/front/js/component/characters.js
+++ src/front/js/component/characters.js
@@ -3,16 +3,23 @@ import { PropTypes } from "prop-types";
 
 
 
+
 const Characters = (props) => {
+  const getInfo =  async (index) => {
+    //const response = await fetch (`https://www.swapi.tech/api/people/${index}`)
+    console.log("hola")
+    //const people = await response.json()
+    //console.log(people)
+  }
   return (
   <>
      
-    <div class="card-group">
-  <div class="card">
+    <div className="card-group">
+  <div className="card">
   <img src="https://starwars-visualguide.com/assets/img/characters/1.jpg" />
-    <div class="card-body">
-      <h5 class="card-title">Luke Skywalker</h5>
-      <button type="button" className="btn btn-secondary">Detalles</button>
+    <div className="card-body">
+      <h5 className="card-title">Luke Skywalker</h5>
+      <button type="button" onClick={()=> getInfo(1)} className="btn btn-secondary">Detalles</button>
       <button type="button" className=  " btn btn-outline-danger border-0">
             <i className="far fa-heart"></i>
       </button>
